a4b20d1174915c6b073c3c042119fd0417fca65a
Victor Chizoba <58464257+vheeq1@users.noreply.github.com>
2024-08-19 09:56:11 +0100

    Update rivalz.sh

--- rivalz.sh
+++ rivalz.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
 
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+YELLOW=$(tput setaf 3)
+
+print_command() {
+  echo -e "${BOLD}${YELLOW}$1${RESET}"
+}
+
+cd $HOME
+
+print_command "Installing NVM and Node..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    . "$NVM_DIR/nvm.sh"
+elif [ -s "/usr/local/share/nvm/nvm.sh" ]; then
+    . "/usr/local/share/nvm/nvm.sh"
+else
+    echo "Error: nvm.sh not found!"
+    exit 1
+fi
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+print_command "Using Node version manager (nvm)..."
+nvm install node
+nvm use node
+
+print_command "Installing Rivalz Cli Package..."
+npm i -g rivalz-node-cli
+
+print_command "Running Rivalz Node..."
+rivalz run
